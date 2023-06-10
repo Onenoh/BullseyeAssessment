d32dbb264f2dadf37b1f0e2963b15bfab1499b12
Okwe Anthonia <Okwe.A@Bullseyemanpower.com>
2023-06-10 15:01:54 +0100

    submitted

--- pricing-component-with-toggle-master/pricing-component-with-toggle-master/pricing.js
+++ pricing-component-with-toggle-master/pricing-component-with-toggle-master/pricing.js
@@ -1,14 +1,13 @@
 let checkBox = document.querySelector('#toggle-switch');
-let amounts = document.querySelectorAll('.amount');
+let annualPrices = document.querySelectorAll('.annual-price');
+let monthlyPrices = document.querySelectorAll('.monthly-price');
 
 checkBox.addEventListener('change', function() {
-  amounts.forEach(amount => {
-    if (checkBox.checked) {
-      amount.classList.toggle('hide-yearly', true);
-      amount.classList.toggle('hide-monthly', false);
-    } else {
-      amount.classList.toggle('hide-yearly', false);
-      amount.classList.toggle('hide-monthly', true);
-    }
-  });
+  if (checkBox.checked) {
+    annualPrices.forEach(amount => amount.style.display = 'flex');
+    monthlyPrices.forEach(amount => amount.style.display = 'none');
+  } else {
+    annualPrices.forEach(amount => amount.style.display = 'none');
+    monthlyPrices.forEach(amount => amount.style.display = 'flex');
+  }
 });
